e6585c18d8c78486a60d607c82de65b46eaf8d0e
Francesco Spezzano <francesco.spezzano@outlook.com>
2021-03-30 18:23:16 +0200

    aggiustato event handlers

--- SommeliAr/Views/SignUpPage.xaml.cs
+++ SommeliAr/Views/SignUpPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using SommeliAr.Models;
 using Xamarin.Forms;
 
@@ -15,7 +16,7 @@ namespace SommeliAr.Views
         void SignInProcedure(object sender, EventArgs e)
         {
             User user = new User(Entry_Username.Text, Entry_Password.Text, Entry_Email.Text);
-            if (user.CheckInformation()) 
+            if (user.CheckInformation())
             {
                 DisplayAlert("Login", "Login Success", "Okay");
             }
@@ -24,7 +25,8 @@ namespace SommeliAr.Views
                 DisplayAlert("Login", "Login Not correct, Empty username or password.", "Oke");
             }
 
-            if (user.IsPasswordMatching(Entry_Password.Text, Entry_ConfirmPassword.Text)){
+            if (user.IsPasswordMatching(Entry_Password.Text, Entry_ConfirmPassword.Text))
+            {
 
                 DisplayAlert("Login", "Login Success", "Okay");
             }
@@ -32,8 +34,45 @@ namespace SommeliAr.Views
             {
                 DisplayAlert("Login", "Login Not correct, Empty username or password.", "Oke");
             }
+
 
 
         }
+
+        private void HideButton_Clicked(object sender, EventArgs e)
+        {
+            if (Entry_Username.IsPassword == true)
+            {
+                Entry_Password.IsPassword = false;
+                Entry_ConfirmPassword.IsPassword = false;
+            }
+            else
+            {
+                Entry_Password.IsPassword = true;
+                Entry_ConfirmPassword.IsPassword = true;
+            }
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var email = Entry_Email.Text;
+
+            /* uso emailregex per verificare se la mail inserita ha una formattazione valida*/
+            var emailPattern =
+                "^[\\w!#$%&'*+\\-/=?\\^_`{|}~]+(\\.[\\w!#$%&'*+\\-/=?\\^_`{|}~]+)*@((([\\-\\w]+\\.)+[a-zA-Z]{2,4})|(([0-9]{1,3}\\.){3}[0-9]{1,3}))\\z";
+
+            if (email != null)
+            {
+                if (Regex.IsMatch(email, emailPattern))
+                    ErrorLabelText.Opacity = 0;
+                else
+                    ErrorLabelText.Opacity = 0.7;
+            }
+
+            else
+                ErrorLabelText.Opacity = 0.7;
+
+        }
     }
+
 }
