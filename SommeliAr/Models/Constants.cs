df14af51b336b135aace475d815e77b867b5584a
Francesco Spezzano <francesco.spezzano@outlook.com>
2021-04-03 10:51:30 +0200

    aggiunta backgroundcolor

--- SommeliAr/Models/Constants.cs
+++ SommeliAr/Models/Constants.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using Xamarin.Forms;
+
 namespace SommeliAr.Models
 {
     public class Constants
     {
         public Constants()
         {
+            public static Color BackgroundColor = Color.FromHex("#8b52ff");
         }
     }
 }
