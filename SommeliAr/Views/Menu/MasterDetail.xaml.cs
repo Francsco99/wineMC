4e326214d6d799de2d613f1b68244281c993bde6
Francesco Spezzano <francesco.spezzano@outlook.com>
2021-04-03 10:45:33 +0200

    Modificata pagina principale aggiunta tabbed page aggiuna pagina settings aggiunta pagina scan aggiunta pagina tastes

--- SommeliAr/Views/Menu/MasterDetail.xaml.cs
+++ SommeliAr/Views/Menu/MasterDetail.xaml.cs
@@ -2,25 +2,17 @@
 using System.Collections.Generic;
 using SommeliAr.Models;
 using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+
 
 namespace SommeliAr.Views.Menu
 {
-    public partial class MasterDetail : ContentPage
+    public partial class MasterDetail : TabbedPage
     {
         public MasterDetail()
         {
             InitializeComponent();
         }
-
-        void Avanti_Clicked(System.Object sender, System.EventArgs e)
-        {
-            var result = new Token();
-            if (result != null)
-            {
-                Application.Current.MainPage = new SecundaryPage();
-            }
-        }
-
 
     }
 }
