cc6341566dda26e3d39d76873856cc2236fee2cb
Fratavaglione <yourself3dprinted@gmail.com>
2021-04-13 17:10:42 +0200

    animation

---
+++ SommeliAr/Views/AfterScan.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SommeliAr.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AfterScan : ContentPage
+    {
+        public AfterScan()
+        {
+            InitializeComponent();
+        }
+
+        private void Favourite_Clicked(object sender, EventArgs e)
+        {
+            StarView.PlayAnimation();
+            
+
+        }
+    }
+}
