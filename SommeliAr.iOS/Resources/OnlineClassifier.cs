5498494bb578590f8a59f616b3b1eec012ab1d78
Francesco Spezzano <francesco.spezzano@outlook.com>
2021-04-21 18:32:39 +0200

    modificate icone home settings camera

---
+++ SommeliAr.iOS/Resources/OnlineClassifier.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SommeliAr.Views
+{
+    public class Prediction
+    {
+        public string TagId { get; set; }
+        public string TagName { get; set; }
+        public double Probability { get; set; }
+        public (double, double, double, double) BoundingBox { get; set; }
+    }
+
+    public class Response
+    {
+        public string Id { get; set; }
+        public string Project { get; set; }
+        public string Iteration { get; set; }
+        public DateTime Created { get; set; }
+        public IList<Prediction> Predictions { get; set; }
+    }
+}
