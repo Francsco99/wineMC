f12890316a520886343b4bcae88a87a29c1653e0
Francesco Spezzano <francesco.spezzano@outlook.com>
2021-04-23 11:22:10 +0200

    messaggio di benvenuto

--- SommeliAr/Menu/MyHomePage.xaml.cs
+++ SommeliAr/Menu/MyHomePage.xaml.cs
@@ -1,18 +1,25 @@
-﻿using SommeliAr.Models;
+﻿using System;
+using Firebase.Auth;
+using Newtonsoft.Json;
+using SommeliAr.Models;
 using SommeliAr.ViewModels;
 using SommeliAr.Views;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace SommeliAr.Menu
 {
     public partial class MyHomePage : ContentPage
-        
+
     {
+        public string WebAPIKey = "AIzaSyB8W5Hq33E8rGn0Bn1CFf3-mzZDydeJSyA";
+
         public MyHomePage()
-        {     
+        {
             InitializeComponent();
             //Toglie le righette di separazione delle entry della listview
             my_list_view.SeparatorVisibility = (SeparatorVisibility)1;
+            GetProfileInformationAndRefreshToken();          
         }
 
         //colore violetto chiaro
@@ -20,17 +27,21 @@ namespace SommeliAr.Menu
 
         void Favourites_btn_Clicked(System.Object sender, System.EventArgs e)
         {
+            User_name_lbl.Text = "";
+            Welcome_lbl.Text = "";
             BindingContext = new MyFavoritesPageViewModel();
             my_list_view.SeparatorVisibility = 0;
             History_btn.TextColor = Color.Black;
             History_btn.FontSize = 20;
 
             Favourites_btn.TextColor = myColor;
-            Favourites_btn.FontSize = 30;  
+            Favourites_btn.FontSize = 30;
         }
 
         void History_btn_Clicked(System.Object sender, System.EventArgs e)
         {
+            User_name_lbl.Text = "";
+            Welcome_lbl.Text = "";
             BindingContext = new MyHistoryPageViewModel();
             my_list_view.SeparatorVisibility = 0;
             Favourites_btn.TextColor = Color.Black;
@@ -47,5 +58,25 @@ namespace SommeliAr.Menu
 
         }
 
+        async private void GetProfileInformationAndRefreshToken()
+        {
+            var authProvider = new FirebaseAuthProvider(new FirebaseConfig(WebAPIKey));
+            try
+            {
+                //This is the saved firebaseauthentication that was saved during the time of login
+                var savedfirebaseauth = JsonConvert.DeserializeObject<Firebase.Auth.FirebaseAuth>(Preferences.Get("MyLoginToken", ""));
+                //Here we are Refreshing the token
+                var RefreshedContent = await authProvider.RefreshAuthAsync(savedfirebaseauth);
+                Preferences.Set("MyLoginToken", JsonConvert.SerializeObject(RefreshedContent));
+                //getting user info
+                User_name_lbl.Text ="Hi, " + savedfirebaseauth.User.DisplayName;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                await App.Current.MainPage.DisplayAlert("Alert", "Oh no !  Token expired", "OK");
+            }
+
+        }
     }
 }
