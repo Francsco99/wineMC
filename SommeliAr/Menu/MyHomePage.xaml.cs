1a8c967bd96cb9603c84cb6b70ca4039a62e92eb
Francesco Spezzano <francesco.spezzano@outlook.com>
2021-05-04 22:20:32 +0200

    animazione loading in homepage

--- SommeliAr/Menu/MyHomePage.xaml.cs
+++ SommeliAr/Menu/MyHomePage.xaml.cs
@@ -48,28 +48,35 @@ namespace SommeliAr.Menu
         private async void Favourites_btn_Clicked(System.Object sender, System.EventArgs e)
         {
             DeleteWelcomeLabels();
-
+            my_list_view.IsVisible = false;
             //setting del BindingContext
             MyFavoritesPageViewModel fav = new MyFavoritesPageViewModel();
+            Loading.IsVisible = true;
             await fav.GetInfoAsync();
+            Loading.IsVisible = false;
+            my_list_view.IsVisible = true;
             BindingContext = fav;
             my_list_view.SeparatorVisibility = 0;
-
+          
             //setting del colore in base al tema del dispositivo
             History_btn.SetAppThemeColor(Label.TextColorProperty, Color.Black, Color.White);
 
             History_btn.FontSize = 20;
             Favourites_btn.TextColor = violetto;
             Favourites_btn.FontSize = 30;
+            
         }
 
         async void History_btn_Clicked(System.Object sender, System.EventArgs e)
-        {
+        {           
             DeleteWelcomeLabels();
-
+            my_list_view.IsVisible = false;
             //setting del BindingContext
             MyHistoryPageViewModel his = new MyHistoryPageViewModel();
+            Loading.IsVisible = true;
             await his.GetInfoAsync();
+            Loading.IsVisible = false;
+            my_list_view.IsVisible = true;
             BindingContext = his;
             my_list_view.SeparatorVisibility = 0;
 
