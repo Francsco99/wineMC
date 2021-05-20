65a479cd3c5ad1b88de0584e88218333d0ef308e
Francesco Spezzano <francesco.spezzano@outlook.com>
2021-05-20 19:09:01 +0200

    modificata pagina dettaglio vino

--- SommeliAr/Views/MyListPageDetail.xaml.cs
+++ SommeliAr/Views/MyListPageDetail.xaml.cs
@@ -23,7 +23,7 @@ namespace SommeliAr.Views
             this.voto = Rating;
             MyItemNameShow.Text = Name;
             MyDescriptionShow.Text = Description;
-            Voto.Text = "Rating: " + voto + " / 100";
+            Voto.Text =  voto;
             MyImageCall.Source = new UriImageSource()
             {
                 Uri = new Uri(Source)
