21d030cab36667c2847dadbe452337b744db5c42
Francesco Spezzano <francesco.spezzano@outlook.com>
2021-05-29 14:00:26 +0200

    correzione funzione colori voti

--- SommeliAr/Views/MyFavouritePageDetail.xaml.cs
+++ SommeliAr/Views/MyFavouritePageDetail.xaml.cs
@@ -17,10 +17,9 @@ namespace SommeliAr.Views
         private string dishes { get; set; }
         private string itemImageSrc { get; set; }
         private string voto { get; set; }
-        public MyFavouritePageDetail(string Name, string Description, string SensorialNotes, string ProductionArea, string Dishes, string Source, string Rating)
-        {
-            InitializeComponent();
 
+        private void AndroidFavouriteInitalization()
+        {
             if (Device.RuntimePlatform == Device.Android)
             {
                 Xamarin.Forms.NavigationPage.SetHasNavigationBar(this, false);
@@ -31,25 +30,6 @@ namespace SommeliAr.Views
                     WineNameFavourite.Margin = margin;
                 }
             }
-
-            this.itemName = Name;
-            this.itemDescription = Description;
-            this.sensorialNotes = SensorialNotes;
-            this.productionArea = ProductionArea;
-            this.dishes = Dishes;
-            this.itemImageSrc = Source;
-            this.voto = Rating;
-            MyItemNameShow.Text = Name;
-            ProdArea.Text = ProductionArea;
-            SensNotes.Text =SensorialNotes;
-            MyDishes.Text = Dishes;
-            Voto.Text =  voto;
-            MyImageCall.Source = new UriImageSource()
-            {
-                Uri = new Uri(Source)
-            };
-
-            Round_frame.BackgroundColor = this.SetFrameColor(voto);
         }
 
         private Color SetFrameColor(string voto)
@@ -68,6 +48,32 @@ namespace SommeliAr.Views
 
             else
                 return Color.Green;
+        }
+
+        public MyFavouritePageDetail(string Name, string Description,
+            string SensorialNotes, string ProductionArea,
+            string Dishes, string Source, string Rating)
+        {
+            InitializeComponent();
+            AndroidFavouriteInitalization();
+            this.itemName = Name;
+            this.itemDescription = Description;
+            this.sensorialNotes = SensorialNotes;
+            this.productionArea = ProductionArea;
+            this.dishes = Dishes;
+            this.itemImageSrc = Source;
+            this.voto = Rating;
+            MyItemNameShow.Text = Name;
+            ProdArea.Text = ProductionArea;
+            SensNotes.Text =SensorialNotes;
+            MyDishes.Text = Dishes;
+            Voto.Text =  voto;
+            MyImageCall.Source = new UriImageSource()
+            {
+                Uri = new Uri(Source)
+            };
+
+            Round_frame.BackgroundColor = this.SetFrameColor(voto);
         }
 
         async void Remove_from_fav_btn_Clicked(System.Object sender, System.EventArgs e)
--- SommeliAr/Views/MyListPageDetail.xaml.cs
+++ SommeliAr/Views/MyListPageDetail.xaml.cs
@@ -16,45 +16,6 @@ namespace SommeliAr.Views
         private string dishes { get; set; }
         private string voto { get; set; }
 
-        public MyListPageDetail(string Name,
-            string Description, string SensorialNotes,
-            string ProductionArea, string Dishes, string Source, string Rating)
-        {
-            InitializeComponent();
-
-            if (Device.RuntimePlatform == Device.Android)
-            {
-                Xamarin.Forms.NavigationPage.SetHasNavigationBar(this, false);
-                if (WineName != null)
-                {
-                    Thickness margin = WineName.Margin;
-                    margin.Top = 60;
-                    WineName.Margin = margin;
-                }
-            }
-
-
-            this.itemName = Name;
-            this.itemDescription = Description;
-            this.sensorialNotes = SensorialNotes;
-            this.productionArea = ProductionArea;
-            this.dishes = Dishes;
-            this.itemImageSrc = Source;
-            this.voto = Rating;
-
-            MyItemNameShow.Text = Name;
-            ProdArea.Text = ProductionArea;
-            SensNotes.Text =SensorialNotes;
-            MyDishes.Text = Dishes;
-            Voto.Text =  voto;
-            MyImageCall.Source = new UriImageSource()
-            {
-                Uri = new Uri(Source)
-            };
-
-            Round_frame.BackgroundColor = this.SetFrameColor(voto);
-        }
-
         private Color SetFrameColor(string voto)
         {
             double votoDouble = Convert.ToDouble(voto); // non funziona bene con i voti puntati: es 7.2
@@ -69,19 +30,59 @@ namespace SommeliAr.Views
                 return Color.Gold;
             }
 
-            else 
+            else
             {
                 return Color.Green;
             }
         }
-        
+
+        private void AndroidInitialization()
+        {
+            if (Device.RuntimePlatform == Device.Android)
+            {
+                Xamarin.Forms.NavigationPage.SetHasNavigationBar(this, false);
+                if (WineName != null)
+                {
+                    Thickness margin = WineName.Margin;
+                    margin.Top = 60;
+                    WineName.Margin = margin;
+                }
+            }
+        }
+
+        public MyListPageDetail(string Name,
+            string Description, string SensorialNotes,
+            string ProductionArea, string Dishes, string Source, string Rating)
+        {
+            InitializeComponent();
+            AndroidInitialization();
+            this.itemName = Name;
+            this.itemDescription = Description;
+            this.sensorialNotes = SensorialNotes;
+            this.productionArea = ProductionArea;
+            this.dishes = Dishes;
+            this.itemImageSrc = Source;
+            this.voto = Rating;
+
+            MyItemNameShow.Text = Name;
+            ProdArea.Text = ProductionArea;
+            SensNotes.Text = SensorialNotes;
+            MyDishes.Text = Dishes;
+            Voto.Text = voto;
+            MyImageCall.Source = new UriImageSource()
+            {
+                Uri = new Uri(Source)
+            };
+
+            Round_frame.BackgroundColor = SetFrameColor(voto);
+        }
+
         async void Add_to_fav_btn_Clicked(System.Object sender, System.EventArgs e)
         {
             new Command(async () =>
             await DBFirebase.Instance.AddFavWine(itemName, Preferences.Get("UserEmailFirebase", ""))).Execute(null);
 
             await DisplayAlert("Success!", itemName + " added correctly", "Ok");
-
         }
     }
 }
